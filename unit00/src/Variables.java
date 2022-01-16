b2c1ec102d6cfa6aa6860f07c700db9cd2a3cf22
Karen <kbei5234@gmail.com>
2022-01-16 10:41:33 -0800

    completed variables code

--- unit00/src/Variables.java
+++ unit00/src/Variables.java
@@ -1,9 +1,9 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
+//Name - Karen Bei
+//Date - 1/16/22
+//Class - APCSA P1
 //Lab  -
 
 public class Variables
@@ -18,25 +18,46 @@ public class Variables
 
 		//integer variables
 		byte byteOne = 127;
+		short shortOne = -24193;
+		int intOne = 91;
+		long longOne = 573819024;
 
 
 		//decimal variables
-
+		float floatOne = 629.391f;
+		double doubleOne = 1023.4015;
 
 		//other integer types
-
+		char charOne = 'k';
 
 		//other types
-
+		boolean booleanOne = true;
+		String stringOne = "this is apcsa";
 
 		//output your information here
 		System.out.println("/////////////////////////////////");
+		System.out.println("*Karen Bei               1/16/22*");
 		System.out.println("*                               *");
 		System.out.println("*        integer types          *");
 		System.out.println("*                               *");
 		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
-
-
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*          real types           *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - floatOne = "+floatOne+"\t*");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+" *");
+		System.out.println("*                               *");
+		System.out.println("*      other integer types      *");
+		System.out.println("*                               *");
+		System.out.println("*16 bit - charOne = "+charOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*          other types          *");
+		System.out.println("*                               *");
+		System.out.println("*booleanOne = "+booleanOne+"\t\t*");
+		System.out.println("*stringOne = "+stringOne+"\t*");
 
 
 
