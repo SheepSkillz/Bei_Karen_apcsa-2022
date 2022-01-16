1d3c77d695968ed57597a330a0f1d7e81c0dbecc
Karen <kbei5234@gmail.com>
2022-01-16 10:27:25 -0800

    completed ascii art code

--- unit00/src/AsciiArt.java
+++ unit00/src/AsciiArt.java
@@ -11,13 +11,26 @@ public class AsciiArt
 	public static void main ( String[] args )
 	{
 		System.out.println("Your Name \n\n" );
+		System.out.println("Karen Bei \n\n");
 		System.out.println("What type of ANIMAL YOU WILL DRAW" );
 		System.out.println("\n\n\n\n" );
 
-		System.out.println("                /\\                " );
-		System.out.println("               /  \\               " );
-		System.out.println("              /    \\              " );
-		System.out.println("             [------]             " );
+		System.out.println("        /\\ _______________ /\\ " );
+		System.out.println("       /                     \\ " );
+		System.out.println("      |        O      O       |     " );
+		System.out.println("      |           /\\          |       " );
+		System.out.println("      |         ______        |      " );
+		System.out.println("      |          \\__/         |      " );
+		System.out.println("       \\ ___________________ /     " );
+		System.out.println("            |           |            " );
+		System.out.println("           /        **   \\          " );
+		System.out.println("          / /|      ** |\\ \\  " );
+		System.out.println("          -  |         |  - " );
+		System.out.println("             |         |    " );
+		System.out.println("             |   ___   |    " );
+		System.out.println("             |  |   |  |    " );
+		System.out.println("             |__|   |__|    " );
+		
 		//add other output
 
 		System.out.println(" \n\n\n\nHelpFul Hints" );
