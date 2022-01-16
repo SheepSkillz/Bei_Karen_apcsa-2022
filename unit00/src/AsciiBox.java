b5a23c71a287cf4f148063f99b77b0e4522a57b1
Karen <kbei5234@gmail.com>
2022-01-16 10:04:04 -0800

    completed ascii box code

--- unit00/src/AsciiBox.java
+++ unit00/src/AsciiBox.java
@@ -10,7 +10,20 @@ public class AsciiBox
 {
 	public static void main(String[] args)
 	{
-		System.out.println("name \t  date \n\n" );
+		System.out.println("name \t\t  date \n\n" );
+		System.out.println("Karen Bei \t  1/16/22 \n\n");
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("ABABABABABABABABABABABABA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("cdefcdefcdefcdefcdefcdefc " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("GhIgHiGhIgHiGhIgHiGhIgHiG " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("++++jklmnopqrstuvwxys++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("------------------------- " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("++012345678909876543210++ " );
 		System.out.println("+++++++++++++++++++++++++ " );
 
 	}
