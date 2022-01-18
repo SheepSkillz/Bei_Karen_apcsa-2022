b572f432bba9249acde7cd7256990deaec7105e0
Karen <kbei5234@gmail.com>
2022-01-18 09:39:11 -0800

    initial push of instruction examples of unit02 and completed circle and cone code

---
+++ unit02/src/Circle.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class Circle
+{
+	public static double area( double radius )
+	{
+		return Math.PI * radius * radius;
+	}
+}
---
+++ unit02/src/Cone.java
@@ -0,0 +1,15 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Cone
+{
+	public static double volume( double radius, int height)
+	{		
+		return (double) Math.PI * radius * radius * height / 3;
+		// unable to write fractions as 1/3
+		// so how do you write a fraction in java??
+	}
+}
