df5eb2731671eabf4ddc267a920fca63a9ffa8d5
Karen <kbei5234@gmail.com>
2022-01-23 22:03:08 -0800

    completed number verify code and test cases in runner

--- unit04/src/NumberVerifyRunner.java
+++ unit04/src/NumberVerifyRunner.java
@@ -13,10 +13,39 @@ public class NumberVerifyRunner
 {
 	public static void main ( String[] args )
 	{
+		Scanner myScanner = new Scanner(System.in);
+		
 		//add in input
-		System.out.println("5 is odd :: " + NumberVerify.isOdd(5));
-		System.out.println("5 is even :: " + NumberVerify.isEven(5));
+		System.out.println("Enter a whole number :: ");
+		int num = myScanner.nextInt();
+		System.out.println(num + " is odd :: " + NumberVerify.isOdd(num));
+		System.out.println(num + " is even :: " + NumberVerify.isEven(num));
+		System.out.println();
 		
 		//add in more test cases
+		System.out.println("Enter a whole number :: ");
+		num = myScanner.nextInt();
+		System.out.println(num + " is odd :: " + NumberVerify.isOdd(num));
+		System.out.println(num + " is even :: " + NumberVerify.isEven(num));
+		System.out.println();
+		
+		System.out.println("Enter a whole number :: ");
+		num = myScanner.nextInt();
+		System.out.println(num + " is odd :: " + NumberVerify.isOdd(num));
+		System.out.println(num + " is even :: " + NumberVerify.isEven(num));
+		System.out.println();
+		
+		System.out.println("Enter a whole number :: ");
+		num = myScanner.nextInt();
+		System.out.println(num + " is odd :: " + NumberVerify.isOdd(num));
+		System.out.println(num + " is even :: " + NumberVerify.isEven(num));
+		System.out.println();
+		
+		System.out.println("Enter a whole number :: ");
+		num = myScanner.nextInt();
+		System.out.println(num + " is odd :: " + NumberVerify.isOdd(num));
+		System.out.println(num + " is even :: " + NumberVerify.isEven(num));
+		System.out.println();
+		
 	}
 }
