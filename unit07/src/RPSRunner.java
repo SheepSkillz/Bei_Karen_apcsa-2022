faad498fcd00b9e50b1273e541e1e8b2c4523645
Karen <kbei5234@gmail.com>
2022-02-07 07:58:55 -0800

    completed RPS runner and code

--- unit07/src/RPSRunner.java
+++ unit07/src/RPSRunner.java
@@ -12,15 +12,40 @@ public class RPSRunner
 		Scanner keyboard = new Scanner(System.in);
 		char response;
 		
+		// run at least once
+		System.out.println("Rock-Paper-Scissors - pick your weapon [R,P,S] :: ");
+
+		String player = "";
+		
+		//read in the player value
+		player = keyboard.next();
+	
+		RockPaperScissors game = new RockPaperScissors();
+		game.setPlayers(player);
+		System.out.println(game.toString());
+		System.out.println();
+		
+		System.out.println("Do you want to play again?");
+		response = keyboard.next().charAt(0);
+		
 		//add in a do while loop after you get the basics up and running
+		do {
+			player = "";
 		
-			String player = "";
-		
-			out.print("type in your prompt [R,P,S] :: ");
+			out.print("pick your weapon [R,P,S] :: ");
 			
 			//read in the player value
+			player = keyboard.next();
 		
-			RockPaperScissors game = new RockPaperScissors();		
+			game.setPlayers(player);
+			System.out.println(game.toString());
+			System.out.println();
+			
+			System.out.println();
+			System.out.println("Do you want to play again?");
+			response = keyboard.next().charAt(0);
+		}
+		while (response == 'y' || response == 'Y');
 	}
 }
 
--- unit07/src/RockPaperScissors.java
+++ unit07/src/RockPaperScissors.java
@@ -12,26 +12,67 @@ public class RockPaperScissors
 
 	public RockPaperScissors()
 	{
-		System.out.println("helloooooo");
+		setPlayers("");
 	}
 
 	public RockPaperScissors(String player)
 	{
+		setPlayers(player);
 	}
 
 	public void setPlayers(String player)
 	{
+		playChoice = player;
 	}
 
 	public String determineWinner()
 	{
 		String winner="";
+		
+		// first get the computer choice through the random function
+		int computer = (int) Math.round(2 * Math.random());
+		if (computer == 0) {
+			compChoice = "R";
+		}
+		if (computer == 1) {
+			compChoice = "P";
+		}
+		if (computer == 2) {
+			compChoice = "S";
+		}
+		
+		// compare the playChoice and compChoice to find winner
+		if (playChoice.equals(compChoice)) {
+			winner = "!Draw Game";
+		}
+		else if (playChoice.equals("R") && compChoice.equals("P")) {
+			winner = "!Computer wins <<Paper Covers Rock>>!";
+		}
+		else if (playChoice.equals("P") && compChoice.equals("S")) {
+			winner = "!Computer wins <<Scissors Cuts Paper>>!";
+		}
+		else if (playChoice.equals("S") && compChoice.equals("R")) {
+			winner = "!Computer wins <<Rocks Breaks Scissors>>!";
+		}
+		else if (playChoice.equals("P") && compChoice.equals("R")) {
+			winner = "!Player wins <<Paper Covers Rock>>!";
+		}
+		else if (playChoice.equals("S") && compChoice.equals("P")) {
+			winner = "!Player wins <<Scissors Cuts Paper>>!";
+		}
+		else if (playChoice.equals("R") && compChoice.equals("S")) {
+			winner = "!Player wins <<Rocks Breaks Scissors>>!";
+		}
+			
 		return winner;
 	}
 
 	public String toString()
 	{
 		String output="";
+		String winner = determineWinner();
+		output += "player had " + playChoice + "\n" + 
+				"computer had " + compChoice + "\n" + winner;
 		return output;
 	}
 }
